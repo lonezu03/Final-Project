42473c83eedf5afe3049dc02f9de9081a193835d
eduongcoder <truongthaiduong0808@gmail.com>
2025-07-08 22:28:44 +0700

    Change call back zalopay fe url

--- src/main/java/com/example/demo/service/ZaloPayService.java
+++ src/main/java/com/example/demo/service/ZaloPayService.java
@@ -69,42 +69,41 @@ public class ZaloPayService {
 	public void checkCallbackGet() {
 		log.info("hihihihi");
 	}
-//	ResponseEntity<?>
+
+	// ResponseEntity<?>
 	public ZaloPayPaymentResponse createPaymentOrderupdate(ZaloPayRequest user) throws JsonProcessingException {
 		HttpHeaders headers = new HttpHeaders();
 		headers.set("Content-Type", "application/x-www-form-urlencoded");
 
-         String url = "https://ba99-2405-4803-d74c-5e60-610a-c3d7-3d42-185e.ngrok-free.app";
-		
+		String url = "https://webtruyen-e79mt48so-phan-thanh-vus-projects.vercel.app";
 
-         int amount = user.getAmount();
-         double voucherPercent = user.getVoucher() != null ? user.getVoucher() : 0.0;
+		int amount = user.getAmount();
+		double voucherPercent = user.getVoucher() != null ? user.getVoucher() : 0.0;
 
-         if (voucherPercent >= 100.0 || voucherPercent <0.0) {
-             throw new IllegalArgumentException("Voucher phần trăm không hợp lệ. Tối đa là 99.9%. Tối thiểu 0.0%");
-         }
+		if (voucherPercent >= 100.0 || voucherPercent < 0.0) {
+			throw new IllegalArgumentException("Voucher phần trăm không hợp lệ. Tối đa là 99.9%. Tối thiểu 0.0%");
+		}
 
-         double pricePerCoin = 1000 * (1 - voucherPercent / 100.0); // Giá 1 coin sau giảm
-         int coinDeposit = (int) (amount / pricePerCoin);
+		double pricePerCoin = 1000 * (1 - voucherPercent / 100.0); // Giá 1 coin sau giảm
+		int coinDeposit = (int) (amount / pricePerCoin);
 
+		log.info("💰 Tính được coinDeposit = {}", coinDeposit);
 
-         log.info("💰 Tính được coinDeposit = {}", coinDeposit);
-         
-		HistoryDepositCreationRequest request = HistoryDepositCreationRequest.builder().amountDeposit(amount).coinDeposit(coinDeposit).voucher(user.getVoucher())
+		HistoryDepositCreationRequest request = HistoryDepositCreationRequest.builder().amountDeposit(amount)
+				.coinDeposit(coinDeposit).voucher(user.getVoucher())
 				.idUser(user.getIdUser()).statusDeposit(StatusDeposit.PENDING).typeDeposit(TypeDeposit.BUY_COIN)
 				.detail(user.getOrderInfo()).build();
 
 		HistoryDepositRespone depositRespone = historyDepositService.createHistoryDeposit(request);
 
-		
 		// Tạo embed_data JSON
 		JSONObject embedData = new JSONObject();
-		String redirectUrl = url+"/payment/callback-success?idHistoryDeposit=" 
-                + depositRespone.getIdHistoryDeposit();
-embedData.put("redirecturl", redirectUrl);
+		String redirectUrl = url + "/payment/callback-success?idHistoryDeposit="
+				+ depositRespone.getIdHistoryDeposit();
+		embedData.put("redirecturl", redirectUrl);
 
 		embedData.put("callbackurl",
-				url+"/api/payment/call");
+				url + "/api/payment/call");
 
 		String embedDataStr = embedData.toString();
 		log.info("📦 embed_data gửi đi: {}", embedDataStr);
@@ -112,7 +111,7 @@ embedData.put("redirecturl", redirectUrl);
 		map.add("app_id", "2553");
 		map.add("key1", key1);
 		map.add("key2", key2);
-		map.add("amount", user.getAmount()+"");
+		map.add("amount", user.getAmount() + "");
 		map.add("app_user", "NovelWebsiteDemo");
 		map.add("embed_data", embedDataStr);
 		map.add("item",
@@ -138,40 +137,43 @@ embedData.put("redirecturl", redirectUrl);
 		ResponseEntity<?> response = restTemplate.exchange(
 				"https://zlpdev-mi-zlpdemo.zalopay.vn/zlp-demo/v2/api/gateway", HttpMethod.POST, entity, String.class);
 
-		 // Parse response.getBody() từ String sang JsonNode
+		// Parse response.getBody() từ String sang JsonNode
 		ObjectMapper objectMapper = new ObjectMapper();
 		JsonNode rootNode = objectMapper.readTree(response.getBody().toString());
 
+		// Parse tiếp request_data và response_data vì chúng là chuỗi JSON lồng trong
+		JsonNode requestData = objectMapper.readTree(rootNode.get("request_data").asText());
+		JsonNode responseData = objectMapper.readTree(rootNode.get("response_data").asText());
 
-	    // Parse tiếp request_data và response_data vì chúng là chuỗi JSON lồng trong
-	    JsonNode requestData = objectMapper.readTree(rootNode.get("request_data").asText());
-	    JsonNode responseData = objectMapper.readTree(rootNode.get("response_data").asText());
+		// Gộp vào 1 Map hoặc Object tùy bạn
+		Map<String, JsonNode> zaloPayResponseMap = new HashMap<>();
+		zaloPayResponseMap.put("request_data", requestData);
+		zaloPayResponseMap.put("response_data", responseData);
 
-	    // Gộp vào 1 Map hoặc Object tùy bạn
-	    Map<String, JsonNode> zaloPayResponseMap = new HashMap<>();
-	    zaloPayResponseMap.put("request_data", requestData);
-	    zaloPayResponseMap.put("response_data", responseData);
-		
 		// Trả về cả phản hồi ZaloPay và id lịch sử
-	    ZaloPayPaymentResponse result = new ZaloPayPaymentResponse(zaloPayResponseMap, depositRespone.getIdHistoryDeposit());
+		ZaloPayPaymentResponse result = new ZaloPayPaymentResponse(zaloPayResponseMap,
+				depositRespone.getIdHistoryDeposit());
 
 		return result;
 	}
 
-//    public String createPaymentOrder(@RequestBody ZaloPayRequest request) throws JsonProcessingException {
-//    	ResponseEntity<?> responseEntity =createPaymentOrderupdate(request);
-//
-//        JsonNode rootNode = objectMapper.readTree(responseEntity.getBody().toString());
-//
-//        String responseDataString = rootNode.get("response_data").asText();
-//
-//        ZaloPayResponseData responseData = objectMapper.readValue(responseDataString, ZaloPayResponseData.class);
-//
-//        System.out.println("Mã giao dịch: " + responseData.getZp_trans_token());
-//        System.out.println("Link thanh toán: " + responseData.getOrder_url());
-//
-//
-//        return responseData.getOrder_url();
-//    }
+	// public String createPaymentOrder(@RequestBody ZaloPayRequest request) throws
+	// JsonProcessingException {
+	// ResponseEntity<?> responseEntity =createPaymentOrderupdate(request);
+	//
+	// JsonNode rootNode =
+	// objectMapper.readTree(responseEntity.getBody().toString());
+	//
+	// String responseDataString = rootNode.get("response_data").asText();
+	//
+	// ZaloPayResponseData responseData = objectMapper.readValue(responseDataString,
+	// ZaloPayResponseData.class);
+	//
+	// System.out.println("Mã giao dịch: " + responseData.getZp_trans_token());
+	// System.out.println("Link thanh toán: " + responseData.getOrder_url());
+	//
+	//
+	// return responseData.getOrder_url();
+	// }
 
 }
