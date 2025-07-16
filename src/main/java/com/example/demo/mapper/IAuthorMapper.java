3e6c38218a61d918777d12648f5c905ef558f839
eduongcoder <truongthaiduong0808@gmail.com>
2025-07-17 01:08:09 +0700

    chỉnh xíu update author

--- src/main/java/com/example/demo/mapper/IAuthorMapper.java
+++ src/main/java/com/example/demo/mapper/IAuthorMapper.java
@@ -3,6 +3,7 @@ package com.example.demo.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 import com.example.demo.dto.request.AuthorCreationRequest;
 import com.example.demo.dto.request.AuthorUpdateRequest;
@@ -11,7 +12,7 @@ import com.example.demo.dto.respone.AuthorRespone;
 import com.example.demo.entity.Author;
 import com.example.demo.entity.User;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface IAuthorMapper {
 	@Mapping(target = "novels", ignore = true)
 	Author toAuthor(AuthorCreationRequest request);
@@ -20,9 +21,9 @@ public interface IAuthorMapper {
 	Author toAuthorUpdate(AuthorUpdateRequest request);
 
 	@Mapping(target = "novels", ignore = true)
-	void updateAuthor( AuthorUpdateRequest request,@MappingTarget Author author);
+	void updateAuthor(AuthorUpdateRequest request, @MappingTarget Author author);
 
-	
-//	@Mapping(target = "novels", ignore = true) // Báo MapStruct đừng đụng đến trường này
+	// @Mapping(target = "novels", ignore = true) // Báo MapStruct đừng đụng đến
+	// trường này
 	AuthorRespone toAuthorRespone(Author author);
 }
